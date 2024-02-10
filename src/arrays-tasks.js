ee7cc48980f9455183e229206f7cf5878dfb58fb
Sergei <106452081+Fidyi@users.noreply.github.com>
2024-02-10 12:27:31 +0300

    comtinue

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -398,8 +398,13 @@ function getElementByIndices(/* arr, indices */) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((acc, e) => {
+    if (!e) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
 }
 
 /**
@@ -435,8 +440,10 @@ function getIdentityMatrix(/* n */) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .map((el, index) => (el % 2 !== 0 ? index : false))
+    .filter((el) => el !== false);
 }
 
 /**
